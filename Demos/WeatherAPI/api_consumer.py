d732b84d3b53f7442094d442f4bdb98761c276fc
rbral <rbral@student.touro.edu>
2025-03-27 14:32:41 -0400

    fixed input

--- Demos/WeatherAPI/api_consumer.py
+++ Demos/WeatherAPI/api_consumer.py
@@ -9,7 +9,6 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
-# city = "Passaic"
 
 def get_weather(city):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
