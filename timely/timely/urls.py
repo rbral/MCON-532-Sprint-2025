77b19e2f9d19e23b2f1797a38afe4cfd0dbb7010
rbral <rbral@student.touro.edu>
2025-05-08 19:40:06 -0400

    merged to main

--- timely/timely/urls.py
+++ timely/timely/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for timely project.
+URL configuration for Timely project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from chat.views import logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chat.urls'))
+    path('', include('chat.urls')),
+
 ]
+
